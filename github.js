0e744b474d711399cfcb49abb85fbef618499157
Nick Verlinden <nick@studiokitara.com>
2017-07-10 21:12:23 +0200

    Cache working.

--- github.js
+++ github.js
@@ -65,6 +65,103 @@
                     return;
                 }
 
+                function ghDone(release) {
+                    if (ghEnableCache) {
+                        //var cacheVolume = config.getVolume();
+                        config.getVolume().then(function(cacheVolume) {
+                            cacheVolume.query(PATH_CACHE + selector.package + "*." + EXT_PKX).then(function(uriList) {
+                                console.log("DEBUG", uriList);
+                                var cache = {};
+                                for (var u in uriList) {
+                                    if (uriList[u].path.lastIndexOf("/") != uriList[u].path.length - 1) {
+                                        var file = uriList[u].path.substr(uriList[u].path.lastIndexOf("/") + 1);
+                                        cache[file.substr(0,file.length - 4)] = uriList[u];
+                                    }
+                                }
+
+                                // get highest version from cache
+                                var highestCache = version.find(cache, selector.package, selector.upgradable || version.UPGRADABLE_NONE);
+                                if (highestCache) {
+                                    highestCache = highestCache.path;
+                                }
+
+                                if (!release) {
+                                    // resolve highest cache version
+                                    resolveURI(highestCache);
+                                }
+                                else {
+                                    var id = selector.package + "." + release.tag_name;
+                                    var found;
+                                    for (var u in cache) {
+                                        if (u == id) {
+                                            found = u;
+                                            break;
+                                        }
+                                    }
+                                    if (found) {
+                                        // release version from github is present in cache
+                                        resolveURI(cache[found]);
+                                    }
+                                    else {
+                                        // download new uri and save to cache
+                                        io.URI.open(release.tarball_url).then(function(repoStream) {
+                                            function repoFail() {
+                                                repoStream.close().then(repoResolve, repoResolve);
+                                            }
+                                            function repoResolve() {
+                                                resolveURI(release.tarball_url);
+                                            };
+                                            
+                                                cacheURI = cacheVolume.getURI(PATH_CACHE + id + "." + EXT_PKX);
+                                                cacheURI.open(io.ACCESS_OVERWRITE, true).then(function(cacheStream) {
+                                                    function cacheFail() {
+                                                        cacheStream.close().then(repoFail, repoFail);
+                                                    }
+                                                    function cacheResolve() {
+                                                        resolveURI(cacheURI);
+                                                    }
+                                                    repoStream.headers = headers;
+                                                    repoStream.copyTo(cacheStream).then(function() {
+                                                        cacheStream.close().then(function() {
+                                                            repoStream.close().then(cacheResolve, cacheResolve);
+                                                        }, cacheFail);
+                                                    }, cacheFail);
+                                                }, repoFail);
+                                        }, resolveURI);
+                                    }
+                                }
+                            }, function() {
+                                // cache path error
+                                resolveURI(release? release.tarball_url : null);
+                            });
+                        }, function() {
+                            // cache path error
+                            resolveURI(release? release.tarball_url : null);
+                        });
+                    }
+                    else {
+                        resolveURI(release? release.tarball_url : null);
+                    }
+
+                    function resolveURI(uri) {
+                        if (uri && uri.name) {
+                            reject(new Error("An error occured while trying to fetch '" + selector.package + "' from the GitHub repository."));
+                            return;
+                        }
+                        else if (!uri) {
+                            reject(new Error("Couldn't find any suitable release for package '" + selector.package + "' in the GitHub repository."));
+                            return;
+                        }
+                        try {
+                            selector.uri = uri;
+                            resolve({"strip": 1, "headers" : headers});
+                        }
+                        catch (e) {
+                            reject(e);
+                        }
+                    }
+                }
+
                 var uriReleases = selector.parseURI(selector.repository.url + URI_PATH_GITHUBAPI_RELEASES_TEMPLATE);
 
                 uriReleases.open().then(function (stream) {
@@ -95,95 +192,10 @@
                         }
 
                         var release = version.find(versions, selector.package, selector.upgradable || version.UPGRADABLE_NONE);
-                        if (ghEnableCache) {
-                            //var cacheVolume = config.getVolume();
-                            config.getVolume().then(function(cacheVolume) {
-                                cacheVolume.exists(PATH_CACHE + selector.package + "*." + EXT_PKX).then(function(uriList) {
-                                    // get highest version from cache
-                                    var highestCache = version.find(uriList, selector.package, selector.upgradable || version.UPGRADABLE_NONE);
-
-                                    if (!release) {
-                                        // resolve highest cache version
-                                        resolveURI(highestCache);
-                                    }
-                                    else {
-                                        var id = selector.package + "." + release.tag_name;
-                                        var found;
-                                        for (var u in uriList) {
-                                            var lastIdx = u.lastIndexOf("/");
-                                            if (lastIdx < 0) {
-                                                continue;
-                                            }
-                                            var fileName = u.substr(lastIdx + 1);
-                                            if (fileName == id + "." + EXT_PKX) {
-                                                found = u;
-                                                break;
-                                            }
-                                        }
-                                        if (found) {
-                                            // release version from github is present in cache
-                                            resolveURI(found);
-                                        }
-                                        else {
-                                            // download new uri and save to cache
-                                            io.URI.open(release.tarball_url).then(function(repoStream) {
-                                                function repoFail() {
-                                                    repoStream.close().then(repoResolve, repoResolve);
-                                                }
-                                                function repoResolve() {
-                                                    resolveURI(release.tarball_url);
-                                                };
-                                                
-                                                    cacheURI = cacheVolume.getURI(PATH_CACHE + id + "." + EXT_PKX);
-                                                    cacheURI.open(io.ACCESS_OVERWRITE, true).then(function(cacheStream) {
-                                                        function cacheFail() {
-                                                            cacheStream.close().then(repoFail, repoFail);
-                                                        }
-                                                        function cacheResolve() {
-                                                            resolveURI(cacheURI);
-                                                        }
-                                                        repoStream.headers = headers;
-                                                        repoStream.copyTo(cacheStream).then(function() {
-                                                            cacheStream.close().then(function() {
-                                                                repoStream.close().then(cacheResolve, cacheResolve);
-                                                            }, cacheFail);
-                                                        }, cacheFail);
-                                                    }, repoFail);
-                                            }, resolveURI);
-                                        }
-                                    }
-                                }, function() {
-                                    // cache path error
-                                    resolveURI(release? release.tarball_url : null);
-                                });
-                            }, function() {
-                                // cache path error
-                                resolveURI(release? release.tarball_url : null);
-                            });
-                        }
-                        else {
-                            resolveURI(release? release.tarball_url : null);
-                        }
-
-                        function resolveURI(uri) {
-                            if (uri && uri.name) {
-                                reject(new Error("An error occured while trying to fetch '" + selector.package + "' from the GitHub repository."));
-                                return;
-                            }
-                            else if (!uri) {
-                                reject(new Error("Couldn't find any suitable release for package '" + selector.package + "' in the GitHub repository."));
-                                return;
-                            }
-                            try {
-                                selector.uri = uri;
-                                resolve({"strip": 1, "headers" : headers});
-                            }
-                            catch (e) {
-                                reject(e);
-                            }
-                        }
-                    }, reject);
-                }, reject);
+                        
+                        ghDone(release);
+                    }, ghDone);
+                }, ghDone);
             });
         };
 
