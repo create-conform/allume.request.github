e7b59578e0f31165f1a6f4384f6bd69286d4673c
Nick Verlinden <nick@studiokitara.com>
2017-07-13 17:55:46 +0200

    Now also saves in namespace subfolder in cache to avoid conflicts.

--- github.js
+++ github.js
@@ -109,17 +109,17 @@
                                 }
 
                                 // get highest version from cache
-                                var highestCache = version.find(cache, selector.package, selector.upgradable || version.UPGRADABLE_NONE);
+                                var highestCache = version.find(cache, direct? direct : selector.package, selector.upgradable || version.UPGRADABLE_NONE);
 
                                 if (!release) {
                                     // resolve highest cache version
                                     resolveURI(highestCache);
                                 }
                                 else {
-                                    var id = (direct? directRepo + "/" + direct : selector.package) + "." + release.tag_name;
+                                    var id = (direct? directRepo + "/" + direct : (selector.repository.namespace + (selector.repository.namespace != ""? "/" : "") + selector.package)) + "." + release.tag_name;
                                     var found;
                                     for (var u in cache) {
-                                        if (u == id) {
+                                        if (u == direct? direct : (selector.package + "." + release.tag_name)) {
                                             found = u;
                                             break;
                                         }
@@ -167,7 +167,7 @@
                                 }
                             }
 
-                            cacheVolume.query(PATH_CACHE + (direct? (directRepo + "/") : "")).then(cacheQueryDone, cacheQueryDone);
+                            cacheVolume.query(PATH_CACHE + (direct? (directRepo + "/") : (selector.repository.namespace + (selector.repository.namespace != ""? "/" : "")))).then(cacheQueryDone, cacheQueryDone);
                         }, function() {
                             // cache path error
                             resolveURI(release? release.tarball_url : null);
@@ -254,7 +254,7 @@
             loader.addRequestProcessor(REQUEST_PROC_NAME, self.process);
         });
     }
-
+    
     var processor = new AllumeRequestGitHub();
     define(function () {
         return processor;
